58c97e5d8355f7a1e3360b73b597252d218a8e99
justACodeKitten <129281130+justACodeKitten@users.noreply.github.com>
2023-04-13 20:36:00 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c19dixrwie9p.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "fongzhijun-employee"
+customregion = "us-east-1"
